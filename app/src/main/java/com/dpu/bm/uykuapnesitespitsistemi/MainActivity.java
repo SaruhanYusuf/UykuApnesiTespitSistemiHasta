e6d4b6a131c0fc974c843ac3ba59fd96c7c1e17b
SaruhanYusuf <101247806+SaruhanYusuf@users.noreply.github.com>
2023-04-02 03:19:01 +0300

    Activiyxml

--- app/src/main/java/com/dpu/bm/uykuapnesitespitsistemi/MainActivity.java
+++ app/src/main/java/com/dpu/bm/uykuapnesitespitsistemi/MainActivity.java
@@ -1,23 +1,18 @@
 package com.dpu.bm.uykuapnesitespitsistemi;
-
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private EditText mailE, sifreE;
@@ -62,6 +57,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAuth.signOut();
+    }
+
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
